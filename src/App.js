278bde595193f23d678d27fa3e01ed97fdb42a4e
amwebexpert <amwebexpert@gmail.com>
2021-03-31 16:26:10 -0400

    Auth0 added logout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import "./App.css";
 import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/Profile";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="app-header">
+        <h2>SPA - React app integrating Auth0 lib</h2>
         <div>Login and logout through OAuth2 flow</div>
+      </header>
+
+      <div className="actions">
         <LoginButton />
         <LogoutButton />
-      </header>
+        <hr />
+      </div>
+
+      <Profile />
     </div>
   );
 }
