a393d5b7b8bb163c8bac24a4862fddfeb1076ab7
amwebexpert <amwebexpert@gmail.com>
2021-03-31 17:22:24 -0400

    Added relative path

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 import "./index.css";
@@ -14,8 +15,10 @@ ReactDOM.render(
       redirectUri={window.location.origin}
       audience="https://amwebexpert.us.auth0.com/api/v2/"
       scope="read:current_user update:current_user_metadata"
-      >
-      <App />
+    >
+      <HashRouter>
+        <App />
+      </HashRouter>
     </Auth0Provider>
     ,
   </React.StrictMode>,
