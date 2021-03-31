d818685c2d2dec0ad9606e7dd14daa2a6cb63fef
amwebexpert <amwebexpert@gmail.com>
2021-03-31 16:13:36 -0400

    Auth0 added logout button

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>Login and logout through OAuth2 flow</div>
+        <LoginButton />
+        <LogoutButton />
       </header>
     </div>
   );
